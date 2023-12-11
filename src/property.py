1fbfd346d972d230d6762b13429ae2528549c471
luzheng <799681071@qq.com>
2023-12-11 10:32:54 +0800

    add author info

--- src/property.py
+++ src/property.py
@@ -1,3 +1,11 @@
+"""
+   @Create Author: Luka
+   @Create Date: 2023-12-11 10:32:23
+   @Last Modified by:   Luka
+   @Last Modified time: 2023-12-11 10:32:23
+"""
+
+
 class Property:
     def __init__(self, method, value) -> None:
         self.method = method
